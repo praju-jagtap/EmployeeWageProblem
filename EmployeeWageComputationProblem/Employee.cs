63a4841bc7a3ccc373e8b719a2a2f20485dbd51b
praju-jagtap <prajaktajagtap2412@gmail.com>
2022-08-23 16:07:13 +0530

    [Prajakta] Add: Using switch Case

--- EmployeeWageComputationProblem/Employee.cs
+++ EmployeeWageComputationProblem/Employee.cs
@@ -9,19 +9,25 @@ namespace EmployeeWageComputationProblem
     internal class Employee
     {
         const int IS_PART_TIME = 1, IS_FULL_TIME = 2, EMP_RATE_PER_HOUR = 20;
-        public void PartTime()
+        public void CalculateEmpWage()
         {
             int empHrs = 0, empWage = 0;
             Random random = new Random();
             int empCheck = random.Next(0, 3);
-            if (empCheck == IS_PART_TIME)
-                empHrs = 4;
-            else if (empCheck == IS_FULL_TIME)
-                empHrs = 8;
-            else
-                empHrs = 0;
+            switch (empCheck) 
+            {
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+            }
             empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("The Part Time Employee And Wage : " + empWage);
+            Console.WriteLine("Using Switch Case The Part Time Employee And Wage is : " + empWage);
         }
     }
 }
--- EmployeeWageComputationProblem/Program.cs
+++ EmployeeWageComputationProblem/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Employee empWage = new Employee();
-            empWage.PartTime();
+            empWage.CalculateEmpWage();
         }
     }
 }
