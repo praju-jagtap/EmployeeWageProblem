0cfbc1857bb2568259a2ae7a4084878b4466c6d4
praju-jagtap <prajaktajagtap2412@gmail.com>
2022-08-23 16:23:32 +0530

    [Prajakta] Add: Calculate Wages For Month

--- EmployeeWageComputationProblem/Employee.cs
+++ EmployeeWageComputationProblem/Employee.cs
@@ -8,26 +8,30 @@ namespace EmployeeWageComputationProblem
 {
     internal class Employee
     {
-        const int IS_PART_TIME = 1, IS_FULL_TIME = 2, EMP_RATE_PER_HOUR = 20;
+        const int IS_PART_TIME = 1, IS_FULL_TIME = 2, EMP_RATE_PER_HOUR = 20, NUM_OF_WORKING_DAY=2;
         public void CalculateEmpWage()
         {
-            int empHrs = 0, empWage = 0;
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-            switch (empCheck) 
+            int empHrs = 0, empWage = 0, totalEmpWage=0;
+            for(int day = 0; day < NUM_OF_WORKING_DAY; day++)
             {
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage += empWage;
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Using Switch Case The Part Time Employee And Wage is : " + empWage);
+            Console.WriteLine("Total Employee Wage For Month : " + totalEmpWage);
         }
     }
 }
